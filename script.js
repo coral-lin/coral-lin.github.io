8f1f33e53bd9c4081fb21fe9f37999d04a45f4b5
coral-lin <corallin31@gmail.com>
2026-09-20 00:00:36 +0800

    修正表單送出方式

--- script.js
+++ script.js
@@ -481,7 +481,7 @@ document.addEventListener('DOMContentLoaded', () => {
         const tempForm = document.createElement('form');
               tempForm.method = 'POST';
               tempForm.action = contactApiUrl;
-              tempForm.target = 'hiddenFormFrame';
+              tempForm.target = '_self';
               tempForm.style.display = 'none';
 
          formData.forEach((value, key) => {
